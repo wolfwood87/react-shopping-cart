0c5f490651759dc7b36a1991ffa575450c905dbb
Derek <legato_5@yahoo.com>
2019-11-13 18:36:17 -0800

    importing context to navigation and shopping cart components. MVP finished.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+//contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -20,15 +22,14 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={{ cart }}>
+				<Navigation />
 
 				{/* Routes */}
 				<Route exact path="/" component= {Products} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+				<Route path="/cart"	component={ShoppingCart} />
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
